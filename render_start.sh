d863b5074a039493edee62201475ea3a7fb308c3
andreasalomone <cuentos.eth@gmail.com>
2025-11-21 12:03:24 +0100

    Fix: Reduce workers to save memory

--- render_start.sh
+++ render_start.sh
@@ -2,10 +2,11 @@
 set -e
 
 echo "Starting Celery Worker..."
-celery -A core.celery_app.celery_app worker --loglevel=info --detach
+celery -A core.celery_app.celery_app worker --loglevel=info --concurrency=1 --detach
 
 echo "Starting Celery Beat..."
 celery -A core.celery_app.celery_app beat --loglevel=info --detach
 
 echo "Starting Gunicorn..."
-exec gunicorn -w 4 -b 0.0.0.0:$PORT app:app
+# Use 2 workers to save memory on Starter plan
+exec gunicorn -w 2 -b 0.0.0.0:$PORT app:app
